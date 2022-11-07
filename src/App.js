be96aa13f696333ef77cf21d477400a38e3594ee
monimunozalzate <106264010+monimunozalzate@users.noreply.github.com>
2022-11-06 23:03:03 -0500

    alerta

--- src/App.js
+++ src/App.js
@@ -15,7 +15,12 @@ function App() {
   };
 
   const calcularResultado = () => {
-    setInput(evaluate(input))
+    if (input) {
+      setInput(evaluate(input))
+    } else{
+      alert("Por favor ingrese valores validos")
+    }
+    
   }
 
   return (
