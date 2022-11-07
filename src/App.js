b10722f4152fa7a80f9f1dcf7f813098ef8f89f0
monimunozalzate <106264010+monimunozalzate@users.noreply.github.com>
2022-11-06 22:51:28 -0500

    1 commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,63 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import Boton from './componentes/Boton';
+import BotonClear from './componentes/BotonClear';
+import Pantalla from './componentes/Pantalla';
+import freecodecampLogo from './imagenes/freeCodeCamp-logo.png'
+import {evaluate} from 'mathjs'
 
 function App() {
+
+  const [input, setInput] = useState('')
+
+  const agregarInput = valor => {
+    setInput(input + valor);
+  };
+
+  const calcularResultado = () => {
+    setInput(evaluate(input))
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='freecodecamp-logo-contenedor'>
+      {/* <img
+        className='freecodecamp-logo'
+        src={freecodecampLogo} 
+        alt="logo de Marvel" /> */}
+    </div>   
+    <div className='contenedor-calculadora'>
+      <Pantalla input={input}/>
+      <div className='fila'>
+        <Boton manejarClic={agregarInput}>1</Boton>
+        <Boton manejarClic={agregarInput}>2</Boton>
+        <Boton manejarClic={agregarInput}>3</Boton>
+        <Boton manejarClic={agregarInput}>+</Boton>
+      </div>
+      <div className='fila'>
+        <Boton manejarClic={agregarInput}>4</Boton>
+        <Boton manejarClic={agregarInput}>5</Boton>
+        <Boton manejarClic={agregarInput}>6</Boton>
+        <Boton manejarClic={agregarInput}>-</Boton>
+      </div>
+      <div className='fila'>
+        <Boton manejarClic={agregarInput}>7</Boton>
+        <Boton manejarClic={agregarInput}>8</Boton>
+        <Boton manejarClic={agregarInput}>9</Boton>
+        <Boton manejarClic={agregarInput}>*</Boton>
+      </div>
+      <div className='fila'>
+      <Boton manejarClic={calcularResultado}>=</Boton>
+      <Boton>0</Boton>
+      <Boton manejarClic={agregarInput}>.</Boton>
+      <Boton manejarClic={agregarInput}>/</Boton>
+      </div>
+      <div className='fila'>
+      <BotonClear manejarClear={()=> setInput('')}>
+        Clear
+      </BotonClear>
+      </div>
+    </div>   
     </div>
   );
 }
---
+++ src/componentes/BotonClear.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import '../hojas-de-estilo/BotonClear.css'
+
+const BotonClear = (props) => {
+  return (
+    <button className='boton-clear' 
+      onClick={props.manejarClear}>
+        {props.children}
+    </button>
+  )
+}
+
+export default BotonClear
